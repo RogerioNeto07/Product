7f6067ad7d10687a1594fef3cd09458da6e6f01e
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2025-02-20 13:58:24 -0300

    atividade finalizada

--- produto/settings.py
+++ produto/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'produto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'product',
+        'USER': 'postgres',
+        'PASSWORD': 'rogerio1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=public'
+        }
     }
 }
 
